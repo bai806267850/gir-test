63176e353da7f97e044be40c4bdb64b5937b8d30
bai <123@123.com>
2018-11-24 23:53:00 +0800

    辅导费

--- git-test/src/com/itheima/TestGig.java
+++ git-test/src/com/itheima/TestGig.java
@@ -4,7 +4,7 @@ public class TestGig {
     public static void main(String[] args) {
         System.out.println("dfsdfsdfsdf");
     }
-
+    //厉害了 呵呵呵
     public void test(){
         int i = 10;
     }
